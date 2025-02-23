2f06c5deaa064c9aa549e7164c134b7b1b7dc484
Spini <viliamchudacik@gmail.com>
2025-02-23 18:12:06 +0100

    Improved discord notification, fixed text for telegram notification, fixed error handling when missing image.

--- octoprint_cvprinting/notifications.py
+++ octoprint_cvprinting/notifications.py
@@ -41,31 +41,33 @@ class Notificationscvprinting:
             embeds[0]["description"] = "Possible issue triggered a warning"
         elif type == "Error":
             embeds[0]["description"] = "Issue triggered a printer pause"
-        response = discord.post(embeds=embeds)
+        response = None
+        if data.get("image"):
+            if os.path.exists(data.get("image")):
+                file={"file": open(data.get("image"), "rb")}
+                file_name=os.path.basename(data.get("image"))
+                embeds[0]["image"] = {"url": f"attachment://{file_name}"}
+                response = discord.post(embeds=embeds, file=file)
+            else:
+                response = discord.post(embeds=embeds)
         if response.status_code not in [200, 204]:
             self._logger.error(f"Error sending discord notification: {response.text}")
             return
-        if data.get("image"):
-            with open(data.get("image"), "rb") as image_file:
-                response = discord.post(file={"file": image_file})
-        if response.status_code != 200:
-            self._logger.error(f"Error sending discord notification: {response.text}")
 
     def notify_telegram(self, type, data):
-        url = f"https://api.telegram.org/bot{self.telegramSettings.get('botToken')}/sendPhoto"
+        url = f"https://api.telegram.org/bot{self.telegramSettings.get('botToken')}/"
         payload = {"chat_id": self.telegramSettings.get("chatId"), "caption": ""}
         if type == "Warning":
-            payload["caption"] = f"CVPrinting: {data.get('message')}\nPossible issue triggered a warning"
+            payload["caption"] = f"CVPrinting: Possible issue triggered a warning"
         elif type == "Error":
-            payload["caption"] = f"CVPrinting: {data.get('message')}\nIssue triggered a printer pause"
+            payload["caption"] = f"CVPrinting: Issue triggered a printer pause"
         #Add image to payload
         response = None
-        if data.get("image"):
-            with open(data.get("image"), "rb") as image_file:
-                files = {"photo": image_file}
-                response = requests.post(url, data=payload, files=files)
+        if data.get("image") and os.path.exists(data.get("image")):
+            files = {"photo": open(data.get("image"), "rb")}
+            response = requests.post(url + "sendPhoto", data=payload, files=files)
         else:
-            response = requests.post(url, data=payload)
-        
+            payload["text"] = payload["caption"]
+            response = requests.post(url + "sendMessage", data=payload)   
         if response and response.status_code != 200:
             self._logger.error(f"Error sending telegram notification: {response.text}")
