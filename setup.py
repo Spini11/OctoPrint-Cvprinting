87d96fd689547591ccf63202994ce923b792fcf8
Spini <viliamchudacik@gmail.com>
2025-04-02 15:18:17 +0200

    Fix for setup.py required packages

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/spini/OctoPrint-Cvprinting"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["MNN","discordwebhook", "PIL"]
+plugin_requires = ["MNN","discordwebhook", "pillow"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
