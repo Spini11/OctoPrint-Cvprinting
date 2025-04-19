ee3e29baa267b2ad7d02f84a2004bccbec6ff63f
Spini <viliamchudacik@gmail.com>
2025-04-19 18:50:22 +0200

    Fixed github link

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Viliam Chudacik (Spini)"
 plugin_author_email = "xchuda06@vutbr.cz"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/spini/OctoPrint-Cvprinting"
+plugin_url = "https://github.com/Spini11/OctoPrint-Cvprinting"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
